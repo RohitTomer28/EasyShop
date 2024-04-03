944b5f8e7a5ce09353f0f70186c4c18f55b57416
RushilJalal <rushiljalal@gmail.com>
2024-04-03 20:31:39 +0530

    hoursheet page

--- EasyShop-main/WindowsFormsApplication1/WindowsFormsApplication1/hoursheet.Designer.cs
+++ EasyShop-main/WindowsFormsApplication1/WindowsFormsApplication1/hoursheet.Designer.cs
@@ -32,6 +32,7 @@
             this.button1 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button2 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -40,54 +41,70 @@
             this.search.BackColor = System.Drawing.Color.Transparent;
             this.search.FlatAppearance.BorderSize = 0;
             this.search.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.search.Location = new System.Drawing.Point(656, 101);
-            this.search.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.search.Location = new System.Drawing.Point(984, 155);
             this.search.Name = "search";
-            this.search.Size = new System.Drawing.Size(135, 56);
+            this.search.Size = new System.Drawing.Size(202, 86);
             this.search.TabIndex = 4;
             this.search.UseVisualStyleBackColor = false;
+            this.search.Click += new System.EventHandler(this.search_Click);
             // 
             // button1
             // 
             this.button1.BackColor = System.Drawing.Color.Transparent;
             this.button1.FlatAppearance.BorderSize = 0;
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button1.Location = new System.Drawing.Point(9, 28);
-            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button1.Location = new System.Drawing.Point(14, 43);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(84, 33);
+            this.button1.Size = new System.Drawing.Size(126, 51);
             this.button1.TabIndex = 5;
             this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(252, 101);
+            this.textBox1.Location = new System.Drawing.Point(378, 155);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(372, 45);
+            this.textBox1.Size = new System.Drawing.Size(556, 67);
             this.textBox1.TabIndex = 6;
             // 
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(78, 176);
+            this.dataGridView1.Location = new System.Drawing.Point(281, 279);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(614, 154);
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.Size = new System.Drawing.Size(637, 237);
             this.dataGridView1.TabIndex = 8;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Red;
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Location = new System.Drawing.Point(445, 554);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(202, 86);
+            this.button2.TabIndex = 9;
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // hoursheet
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::WindowsFormsApplication1.Properties.Resources._11;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1200, 692);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.search);
             this.DoubleBuffered = true;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "hoursheet";
             this.Text = "hoursheet";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
@@ -102,5 +119,6 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button2;
     }
 }
--- EasyShop-main/WindowsFormsApplication1/WindowsFormsApplication1/hoursheet.cs
+++ EasyShop-main/WindowsFormsApplication1/WindowsFormsApplication1/hoursheet.cs
@@ -7,21 +7,72 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Oracle.ManagedDataAccess.Client;
 
 namespace WindowsFormsApplication1
 {
     public partial class hoursheet : Form
     {
+        OracleConnection conn;
         public hoursheet()
         {
             InitializeComponent();
         }
-
+        public void ConnectDB()
+        {
+            conn = new OracleConnection("Data Source=localhost:1521;Persist Security Info=True;User ID=system;Password=1234");
+            conn.Open();
+        }
         private void button1_Click(object sender, EventArgs e)
         {
             emp_det emp_Det = new emp_det();
             this.Close();
             emp_Det.Show();
+        }
+
+        private void search_Click(object sender, EventArgs e)
+        {
+            
+            
+                ConnectDB();
+
+                // Assuming 'textBoxMonth' is the TextBox where the month is entered.
+                string month = textBox1.Text;
+
+                // Modify the SQL command to filter by the month.
+                // The 'TO_CHAR' function is used to extract the month part from the date.
+                // Ensure the date format 'MM' matches the format in which the month is entered.
+                using (OracleCommand cmd = new OracleCommand("SELECT * FROM employee_hours WHERE TO_CHAR(w_date, 'MM') = :month", conn))
+                {
+                    // Add the parameter to the command to filter by month
+                    cmd.Parameters.Add(new OracleParameter("month", month));
+
+                    using (OracleDataAdapter oda = new OracleDataAdapter(cmd))
+                    {
+                        DataTable dt = new DataTable();
+                        oda.Fill(dt);
+                        dataGridView1.DataSource = dt; // 'dataGridView1' is assumed to be the DataGridView
+                    }
+                }
+                conn.Close();
+            
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ConnectDB();
+
+            using (OracleCommand cmd = new OracleCommand("Select * from employee_hours", conn))
+            {
+                using (OracleDataAdapter oda = new OracleDataAdapter(cmd))
+                {
+                    DataTable dt = new DataTable();
+                    oda.Fill(dt);
+                    dataGridView1.DataSource = dt;
+                }
+            }
+            conn.Close();
         }
     }
 }
